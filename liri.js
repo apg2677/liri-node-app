4c8a6f85e4a30caf3478b8039210b363ccd98a98
Amir P <apourrezaei@gmail.com>
2019-04-17 18:55:21 -0400

    NPM Packages

---
+++ liri.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+var keys = require(".keys.js");
+
+var spotify = new spotify(keys.spotify);
+
+var commands = ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"];
+
