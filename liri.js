c3a311a5da871d6b0925e363fce7f3dce4b46857
Amir P <apourrezaei@gmail.com>
2019-04-18 14:08:02 -0400

    bands in town

--- liri.js
+++ liri.js
@@ -1,8 +1,18 @@
 require("dotenv").config();
+var axios = require("axios");
 
-var keys = require(".keys.js");
+var Spotify = require("node-spotify-api");
 
-var spotify = new spotify(keys.spotify);
+// var keys = require("./keys.js");
+
+// var spotify = new Spotify(keys.spotify);
+var transact = process.argv[2];
+
 
 var commands = ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"];
+var artist = encodeURI(process.argv[3]);
+var requestUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
+axios.get(requestUrl).then(function(res) {
+    console.log(res.data);
+});
