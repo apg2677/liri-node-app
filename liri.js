de7a9dcf51f0ad2ee8863564eb011356a9be7331
Amir P <apourrezaei@gmail.com>
2019-04-18 18:27:41 -0400

    OMDB

--- liri.js
+++ liri.js
@@ -26,16 +26,43 @@ switch (transact) {
         });
         break;
     case commands[1]:
-        var song = encodeURI(process.argv[3]);
-        spotify.search({ type: 'track', query: song }).then(function(res) {
-            var artist = res.tracks.items[0].artists[0].name;
-            var album = JSON.stringify(res.tracks.items[0].album.name);
-            console.log("Song: " + decodeURI(song));
-            console.log("Artist: " + artist);
-            console.log("Album: " + album); 
+        SongInfo();
+
+    case commands[2]:
+        var movieStr = encodeURI(process.argv[3]);
+        var requestUrl = "http://www.omdbapi.com/?t=" + movieStr + "&y=&plot=short&apikey=trilogy";
+        axios.get(requestUrl).then(function(res) {
+            var title = res.data.Title;
+            var year = res.data.Year;
+            var ratingIMDB = res.data.imdbRating;
+            var ratingRT = res.data.tomatoRating;
+            var lang = res.data.Language;
+            var plot = res.data.Plot;
+            var actors = res.data.Actors;
+            console.log("Title: " + title);
+            console.log("Year: " + year);
+            console.log("IMDB Rating: " + JSON.stringify(ratingIMDB));
+            console.log("Rotten Tomatoes: " + ratingRT);
+            console.log("Language: " + lang);
+            console.log("Plot: " + plot );
+            console.log("Actors: " + JSON.stringify(actors));
         });
 
+
 }
+function SongInfo() {
+    var song = encodeURI(process.argv[3]);
+    spotify.search({ type: 'track', query: song }).then(function (res) {
+        var artist = res.tracks.items[0].artists[0].name;
+        var album = JSON.stringify(res.tracks.items[0].album.name);
+        var prevLink = res.tracks.items[0].preview_url;
+        console.log("Song: " + decodeURI(song));
+        console.log("Artist: " + artist);
+        console.log("Album: " + album);
+        console.log("Preview Link: " + prevLink);
+    });
+}
+
 function ConcertInfo(res, i) {
     var venName = res.data[i].venue.name;
     var city = res.data[i].venue.city;
