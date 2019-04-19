59dc76269b269a7d5c1d86f4e1f4a42217f9b7f3
Amir P <apourrezaei@gmail.com>
2019-04-19 16:19:42 -0400

    search - code cleanup

--- liri.js
+++ liri.js
@@ -37,46 +37,4 @@ switch (transact) {
         });
         break;
     }
-// function MovieInfo() {
-//     axios.get(requestUrl).then(function (res) {
-//         var title = res.data.Title;
-//         var year = res.data.Year;
-//         var ratingIMDB = res.data.imdbRating;
-//         var ratingRT = res.data.tomatoRating;
-//         var lang = res.data.Language;
-//         var plot = res.data.Plot;
-//         var actors = res.data.Actors;
-//         console.log("Title: " + title);
-//         console.log("Year: " + year);
-//         console.log("IMDB Rating: " + JSON.stringify(ratingIMDB));
-//         console.log("Rotten Tomatoes: " + ratingRT);
-//         console.log("Language: " + lang);
-//         console.log("Plot: " + plot);
-//         console.log("Actors: " + JSON.stringify(actors));
-//     });
-// }
-
-// function SongInfo() {
-//     var song = encodeURI(process.argv[3]);
-//     spotify.search({ type: 'track', query: song }).then(function (res) {
-//         console.log(JSON.stringify(res, null, 4));
-//         var artist = res.tracks.items[0].artists[0].name;
-//         var album = JSON.stringify(res.tracks.items[0].album.name);
-//         var prevLink = res.tracks.items[0].preview_url;
-//         console.log("Song: " + decodeURI(song));
-//         console.log("Artist: " + artist);
-//         console.log("Album: " + album);
-//         console.log("Preview Link: " + prevLink);
-//     });
-// }
-
-// function ConcertInfo(res, i) {
-//     var venName = res.data[i].venue.name;
-//     var city = res.data[i].venue.city;
-//     var state = res.data[i].venue.region;
-//     var date = res.data[i].datetime;
-//     console.log("Venue: " + venName);
-//     console.log("Location: " + city + ", " + state);
-//     console.log("Date: " + date);
-// }
 
